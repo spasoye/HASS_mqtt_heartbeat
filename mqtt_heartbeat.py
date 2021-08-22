b2339e2d516ef012cddadef13a0b36ba06fff427
Spasoye <ivan@spasoye.com>
2021-08-22 11:52:07 +0200

    check log size and cfg path

--- mqtt_heartbeat.py
+++ mqtt_heartbeat.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 
+import sys
 import time
 from datetime import datetime
 import json
 import paho.mqtt.client as mqtt
+from pathlib import Path
+import os
 
 def log_write(text):
     global log_file
@@ -16,13 +19,23 @@ def on_connect(client, userdata, rc, *extra_params):
     log_write('Connect result:', str(rc))
     print('Connect result:', str(rc))
 
+if len(sys.argv) != 2:
+    print("Must provide cfg file absolute path")
+    sys.exit()
+
 # import configuration
-cfg_file = open("config.json", "r")
+cfg_file = open(sys.argv[1], "r")
 cfg_str = cfg_file.read()
 cfg_file.close()
 cfg_json = json.loads(cfg_str)
 
 log_file = cfg_json["log_file"]
+
+print("log file size: ", Path(log_file).stat().st_size)
+if Path(log_file).stat().st_size > 200:
+    print("log file to big. Deleting log file")
+    os.remove(log_file)
+
 name = cfg_json["name"]
 broker = cfg_json["broker"]
 period = cfg_json["period"]
