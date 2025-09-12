3d9adede1d2605fd33a7c5e3a3038f9d886966c5
Ivan <ivan@spasoye.com>
2025-09-12 17:21:24 +0200

    Restructured config

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 log_file = "pc_status.log"
-name = "pc"
-broker = "192.168.100.70"
+name = "Workshop PC heartbeat"    # Friendly display name
+unique_id = "workshop_pc_heartbeat"    # Technical identifier
 period = 20
 
+manufacturer = "Spas Tech"
+model = "Heartbeat v1"
+class broker:
+    host = "192.168.100.70"
+    port = 1883
+    username = None
+    password = None
+    use_tls = False
+    tls_key = None
+    tls_certfile = None
+    tls_ca_cert = None
+    discovery_prefix = "homeassistant"
+    state_prefix = "hmd"
+    client = None
